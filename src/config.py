87ae1a75173bc219a9b1e869e41346c8a57d23f4
skvis <vishalksingh97@gmail.com>
2020-06-06 20:17:25 +0530

    code added

---
+++ src/config.py
@@ -0,0 +1,12 @@
+
+DATA_PATH = '../input/data.txt'
+MODEL_PATH = '../models/'
+
+EMBEDDING_DIM = 100
+PAD_TYPE = 'pre'
+
+# BATCH_SIZE = 64
+LEARNING_RATE = 1e-3
+NUM_EPOCHS = 100
+
+GENERATE_WORDS = 100
